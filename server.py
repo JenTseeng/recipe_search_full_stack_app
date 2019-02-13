ab1aab8610f36644478000b7575fcf23ed3b2334
vagrant <vagrant@vagrant.vm>
2019-02-13 23:17:17 +0000

    Functional search page with results

--- server.py
+++ server.py
@@ -6,8 +6,11 @@ from flask import Flask, render_template, request, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
-app.secret_key = os.environ['APIKey']
-
+app.search_id = os.environ['search_id']
+app.search_key = os.environ['search_key']
+app.ingred_id = os.environ['ingred_id']
+app.ingred_key = os.environ['ingred_key']
+app.secret_key = "ABC"
 
 @app.route("/")
 def homepage():
@@ -27,43 +30,59 @@ def show_recipe_search_form():
 def find_recipes():
     """Search for recipes on Spoonacular"""
 
-    query = request.args.get('search_field')
-    print ('This is my query!!!!!!!!!!!!!  :'+query)
-
-    # payload = {'number':5,'ranking':1,'ingredient':'sour cream',
-    #             'headers':{'X-RapidAPI-Key':app.secret_key}}
-    # url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients'
+    # spoonacular setup
     
-    # response = requests.get(url, params=payload)
+    # header = {'X-RapidAPI-Key':app.secret_key}
+    # url = 'https://webknox-recipes.p.rapidapi.com/recipes/findByIngredients'
+    # response = requests.get(url, headers=header, params=payload) #spoonacular
+    
+    query = request.args.get('search_field')
+    excluded = ''
+    num_recipes = 10
+    payload = {'app_id':app.search_id, 'app_key':app.search_key, 
+    'q':query, 'from':0, 'to':num_recipes, 'excluded':excluded}    
+    url = 'https://api.edamam.com/search'
+    
+    response = requests.get(url, params=payload)
+    data = response.json()
 
-    # data = response.json()
-    # - (Make sure to save the JSON data from the response to the data
-    #   variable so that it can display on the page as well.)
+    recipes = parse_search_results(data['hits'])
+    # results = data['hits']
 
-    #data = {'This': ['Some', 'mock', 'JSON']}
-    # events = []
+    
+    return render_template("search_results.html", data=data, recipes=recipes)
 
-    # return render_template("afterparties.html",data=pformat(data), results=events)
-
-    # # If the required info isn't in the request, redirect to the search form
-    # else:
-    #     flash("Please provide all the required information!")
-    #     return redirect("/afterparty-search")
-
+# url= 'https://api.edamam.com/api/nutrition-details'
+# payload = {'app_id':app.ingred_id, 'app_key':app.ingred_key,'ingr':'1 teaspoon apple cider vinegar'}
+# response = requests.get(url, params=payload)
 
 ######### Helper Functions #########
-def get_access_token(code):
-    """Use access code to request user's access token"""
+def parse_search_results(data):
+    """ Parse API returned recipe results and return a list of dictionaries
 
-    # Add your post request here to get an access token
-    # Call add_bookmark with your access token and event_id
-    pass
+    Edamam returns results as a list of hits
+    Each hit has a recipe, bookmarked, and bought
+    Each recipe has: label (ie: recipe title), image, url, yield, ingreds, etc.
+    Each ingredient is a list of dictionaries.  1 dictionary per ingredient
+    Each ingredient dictionary has text and weight (few have quantity and measure)
 
+    """
+    recipe_results = []
+    for hit in data:
+        new_entry = {}
+        ingredients = []
+        recipe = hit['recipe']
+        new_entry['title'] = recipe['label']
+        new_entry['image'] = recipe['image']
 
-def add_bookmark(access_token, event_id):
-    """Use user's access token to save bookmark for event"""
+        # Pull out text from each ingredient entry
+        for ingredient in recipe['ingredients']:
+            ingredients.append(ingredient['text'])        
+        new_entry['ingredients'] = ingredients
 
-    pass
+        recipe_results.append(new_entry)
+
+    return recipe_results
 
 
 if __name__ == "__main__":
